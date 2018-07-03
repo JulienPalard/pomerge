691a1604b166dc92fe3773e0415cba00495e825a
Julien Palard <julien@palard.fr>
2018-07-03 10:15:07 +0200

    Adding dev requirements.

--- setup.py
+++ setup.py
@@ -3,35 +3,34 @@
 
 from setuptools import setup
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
 setup(
-    name='pomerge',
-    version='0.1.1',
+    name="pomerge",
+    version="0.1.1",
     description="Merge known translations between .po files.",
     long_description=readme,
     author="Julien Palard",
-    author_email='julien@palard.fr',
-    url='https://github.com/JulienPalard/pomerge',
-    packages=['pomerge',],
-    package_dir={'pomerge': 'pomerge'},
-    entry_points={
-        'console_scripts': ['pomerge=pomerge.pomerge:main']
-    },
+    author_email="julien@palard.fr",
+    url="https://github.com/JulienPalard/pomerge",
+    packages=["pomerge"],
+    package_dir={"pomerge": "pomerge"},
+    entry_points={"console_scripts": ["pomerge=pomerge.pomerge:main"]},
     include_package_data=True,
-    install_requires=['tqdm', 'polib'],
+    install_requires=["tqdm", "polib"],
+    extras_require={"dev": ["pylint", "flake8", "black", "mypy"]},
     license="MIT license",
     zip_safe=False,
-    keywords='pomerge',
+    keywords="pomerge",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
-    ]
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
 )
